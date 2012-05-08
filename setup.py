7c8ee0b717d49c219b0332ba78dba7d6c7af060c
Jonathan Sick <jonathansick@mac.com>
2012-05-07 23:30:26 -0400

    Added pip-requirements to satisfy RTD

--- setup.py
+++ setup.py
@@ -8,13 +8,14 @@ from sphinx.setup_command import BuildDoc
 
 dependencies = """
 sphinx
+numpy
 pymongo
 """
 
 cmdclass = {'build_sphinx': BuildDoc}
 
 setup(name='moastro',
-    version=0.0,
+    version='0.2',
     author='Jonathan Sick',
     author_email='jonathansick@mac.com',
     description='MongoDB framework for observational astronomers',
